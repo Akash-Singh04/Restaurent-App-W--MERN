6ba81815c51754a848423a78bfe148a410f157b5
Akash <akashsingh2210670@gmail.com>
2023-02-13 21:51:59 +0530

    Initalised Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import SocialIcons from './components/socialicons'
+import ResponsiveAppBar from './components/navbar'
+
+function App() {
+  return (
+    <div>
+    <ResponsiveAppBar />
+    <SocialIcons />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/socialicons.jsx
@@ -0,0 +1,25 @@
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+function SocialIcons(){
+    return(
+        <div className="social_links">
+            <ul>
+                <li>
+                    <a href="https://twitter.com/Kind_Of_Akash"><TwitterIcon /></a>
+                </li>
+                <li>
+                    <a href="https://github.com/Akash-Singh04"><GitHubIcon /></a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/kind.of.akash/"><InstagramIcon /></a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/akash-singh-a57081253/"><LinkedInIcon /></a>
+                </li>
+            </ul>
+        </div>)
+}
+
+export default SocialIcons
